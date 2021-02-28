c9b5945b06fe7a730b4c72874ec48552c0623ce8
mvaisakh <vaisakhmurali@gmail.com>
2021-02-28 07:08:04 +0530

    Import x86_64 GCC 20210228
    Build completed on: Sun Feb 28 07:08:04 IST 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/x86_64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-x86/bin/../libexec/gcc/x86_64-elf/11.0.1/lto-wrapper
    Target: x86_64-elf
    Configured with: ../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-x86 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.0.1 20210228 (experimental) (Eva GCC)
    
    Signed-off-by: mvaisakh <vaisakhmurali@gmail.com>

---
+++ lib/gcc/x86_64-elf/11.0.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=x86_64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-x86 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
